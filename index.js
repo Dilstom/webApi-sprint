d531d711d49ba97923a77d78aa8e41a786f868fc
Dil Sombayeva <dilyasom@gmail.com>
2019-08-01 07:22:41 -0700

    add simple server for the start

--- index.js
+++ index.js
@@ -12,3 +12,10 @@ I need this code, just don't know where, perhaps should make some middleware, do
 
 Go code!
 */
+
+const express = require('express');
+
+const server = express();
+const port = 4007;
+
+server.listen(port, () => console.log(`API is listening on port ${port}`));
