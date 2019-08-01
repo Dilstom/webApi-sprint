71eed6297628ff965bb2edf818cc69b994ff6c96
Dil Sombayeva <dilyasom@gmail.com>
2019-08-01 12:47:54 -0700

    add project get ('/:id')     req

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -14,4 +14,19 @@ router.get('/', (req, res) => {
   });
 });
 
+router.get('/:id', validateId, (req, res) => {
+ res.status(200).json(req.project);
+});
+
+router.post('/', validateBody, (req, res) => {
+ projectsDb
+  .insert(req.body)
+  .then(project => {
+   res.status(201).json(project);
+  })
+  .catch(err => {
+   res.status(500).json(err);
+  });
+});
+
 module.exports = router;
