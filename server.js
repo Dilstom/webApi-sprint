9276f893ea7d27607fa27e76ea17f5a0b4c6103c
Dil Sombayeva <dilyasom@gmail.com>
2019-08-01 12:52:43 -0700

    add actionRouter

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const projectsRouter = require('./projects/projectRouter.js');
+const actionsRouter = require('./actions/actionRouter');
 const server = express();
 
 server.use(express.json());
 server.use('/api/projects', projectsRouter);
+server.use('/api/actions', actionsRouter);
 
 server.get('/', (req, res) => {
  res.send('<h1>Hello Home Page!</h1>');
