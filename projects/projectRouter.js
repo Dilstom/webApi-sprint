959bff1d5b841c102b50354169c33223e089af3c
Dil Sombayeva <dilyasom@gmail.com>
2019-08-01 07:42:11 -0700

    projects - add get ('/) req

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -3,4 +3,15 @@ const projectsDb = require('../data/helpers/projectModel');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+ projectsDb
+  .get()
+  .then(projects => {
+   res.status(200).json(projects);
+  })
+  .catch(err => {
+   res.status(500).json(err);
+  });
+});
+
 module.exports = router;
