2de3ea2a5b47abc571c97c76a2f5959eb8194fd7
Dil Sombayeva <dilyasom@gmail.com>
2019-08-02 10:21:07 -0700

    action delete req

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -17,6 +17,14 @@ router.get('/:id', validateId, (req, res) => {
  res.status(200).json(req.action);
 });
 
-router.post('/:id');
+router.delete('/:id', validateId, (req, res) => {
+ actionDb
+  .remove(req.params.id)
+  .then(action => {
+   res.status(200).json({ message: 'The action has been removed', action });
+  })
+  .catch(err => res.status(500).json(err));
+});
+
 
 module.exports = router;
